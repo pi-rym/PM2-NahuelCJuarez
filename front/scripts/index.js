75e2cd3746914784539628e87f46df350ce70004
Nahuel Juarez <cherai92@gmail.com>
2024-03-06 02:23:53 -0300

    tarea de ajax pendiente por falta de tiempo

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,56 @@
-console.log(tempData);
+//console.log(tempData);
+//console.log("estoy en el index");
+const filmsContainer = document.getElementById("movies")
+
+renderMovies = function(movie) {
+    const movieCard = document.createElement("div")
+    movieCard.classList.add("movie_card")
+    movieCard.id = movie.id;
+
+     const infoSection = document.createElement("div")
+     infoSection.classList.add("info_section")
+     infoSection.style.cursor = "pointer";
+     infoSection.addEventListener("click", function() {
+        window.location.href = "https://www.youtube.com"; 
+    });    
+
+       const divImagen = document.createElement("div")
+       divImagen.classList.add("movie_header")
+
+       const divTextos = document.createElement("div")
+       divTextos.classList.add("movie_desc")
+
+       const movieSocial = document.createElement("div")
+       movieSocial.classList.add("movie_social")
+
+     const blurBack = document.createElement("div")
+     blurBack.classList.add("blur_back", `${movie.id}_back`)
+     
+
+    divImagen.innerHTML = `<img class="locandina" src="${movie.poster}"/>
+    <h1>${movie.title}</h1>
+    <h4>${movie.year}, ${movie.director}</h4>
+    <span class="minutes">${movie.duration}</span>
+    <p class="type">${movie.genre.join(", ")}
+    `
+
+    divTextos.innerHTML = `<p class="text">${movie.description}</p>
+    `
+
+    movieSocial.innerHTML = `<ul>
+    <li><i class="material-icons">${movie.rate}</i></li>
+    </ul>
+    `
+
+  filmsContainer.appendChild(movieCard);
+  movieCard.appendChild(infoSection);
+  movieCard.appendChild(blurBack);
+  infoSection.appendChild(divImagen);
+  infoSection.appendChild(divTextos);
+  infoSection.appendChild(movieSocial);
+}
+
+
+
+
+tempData.forEach(renderMovies)
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -1,31 +1,37 @@
 const tempData = [
   {
+    id: "Gotg",
     title: "Guardians of the Galaxy Vol. 2",
     year: 2017,
     director: "James Gunn",
     duration: "2h 16min",
     genre: ["Action", "Adventure", "Comedy"],
     rate: 7.7,
+    description: "The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father, the ambitious celestial being Ego.",
     poster:
       "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
   },
   {
+    id: "Sw",
     title: "Star Wars: Episode IV - A New Hope",
     year: 1977,
     director: "George Lucas",
     duration: "2h 1min",
     genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
     rate: 8.7,
+    description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
     poster:
       "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
   },
   {
+    id: "Lotr",
     title: "The Lord of the Rings: The Fellowship of the Ring",
     year: 2001,
     director: "Peter Jackson",
     duration: "2h 58min",
     genre: ["Action", "Adventure", "Drama", "Fantasy"],
     rate: 8.8,
+    description: "Set in Middle-earth, the story tells of the Dark Lord Sauron, who seeks the One Ring, which contains part of his might, to return to power.",
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
